66d3be9dc4886b942061239f5be4d1b158441d16
Goel, Pulkit <Pulkit.Goel@darwin.com>
2026-02-09 03:19:06 +0530

    fdsfsd

--- server.js
+++ server.js
@@ -1,24 +1,22 @@
-const { createServer } = require('http')
-const { parse } = require('url')
+const http = require('http')
+const url = require('url')
 const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
 const hostname = process.env.WEBSITE_HOSTNAME || 'localhost'
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 
 const app = next({ dev, hostname, port })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
-  createServer(async (req, res) => {
-    try {
-      const parsedUrl = parse(req.url, true)
-      await handle(req, res, parsedUrl)
-    } catch (err) {
+  http.createServer((req, res) => {
+    const parsedUrl = url.parse(req.url, true)
+    handle(req, res, parsedUrl).catch((err) => {
       console.error('Error occurred handling', req.url, err)
       res.statusCode = 500
       res.end('internal server error')
-    }
+    })
   })
     .once('error', (err) => {
       console.error(err)
